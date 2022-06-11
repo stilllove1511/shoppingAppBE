714a4a9507604941b3226892853f069eb051b6cc
painkiller <commitment1511@outlook.com>
2022-06-11 11:04:20 +0700

    change mongo cloud connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://stilllove:1@cluster0.cvbjf.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://stilllove:1@cluster0.2qqou.mongodb.net/?retryWrites=true&w=majority');
         console.log('connect db succesfully')
     } catch (error) {
         console.log('connect db fail')
