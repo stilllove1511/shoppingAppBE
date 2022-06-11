cd5552b5a6348a367745229b0ddc4e1bfa27e061
painkiller <commitment1511@outlook.com>
2022-06-11 22:26:08 +0700

    404 server.js

--- src/server.js
+++ src/server.js
@@ -1,32 +1,29 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from "express";
+import bodyParser from "body-parser";
 
-import initAppRoutes from './routes/index'
-import db from './config/db'
-import {
-    configCors
-} from './config/cors'
+import initAppRoutes from "./routes/index";
+import db from "./config/db";
+import { configCors } from "./config/cors";
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-const hostname = process.env.HOST_NAME
-const PORT = process.env.PORT || 8080
+const hostname = process.env.HOST_NAME;
+const PORT = process.env.PORT || 8080;
 
-db.connect()
+db.connect();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-configCors(app)
+configCors(app);
 initAppRoutes(app);
 
 app.use((req, res) => {
-    return res.send('nham api roi :(')
-})
+    return res.status(404).send("nham api roi :(");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on site ${hostname}:${PORT}`)
-})
+    console.log(`Server is running on site ${hostname}:${PORT}`);
+});
