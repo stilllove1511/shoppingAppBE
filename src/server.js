b11e94b7b72cac38bac59a58b9c84d780b6355cb
painkiller <commitment1511@outlook.com>
2022-06-11 17:09:27 +0700

    cors config

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser'
 
 import initAppRoutes from './routes/index'
 import db from './config/db'
+import {
+    configCors
+} from './config/cors'
 
 require('dotenv').config()
 
@@ -17,6 +20,7 @@ db.connect()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+configCors(app)
 initAppRoutes(app);
 
 app.use((req, res) => {
